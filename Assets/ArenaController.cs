c2bdf9dfc0151586f62b149829dfd7b4fdbf04e2
WoolfyGor <limoni10@yandex.ru>
2022-11-30 02:06:24 +0300

    End of day commit, start working on randomized arena

---
+++ Assets/ArenaController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaController : MonoBehaviour
+{
+    [SerializeField] GameObject[] Arenas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
